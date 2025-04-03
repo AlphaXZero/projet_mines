b48c33dcd2b39a4acd188723c223f630686d7ea0
gg <georgevanderveen@yahoo.fr>
2025-04-03 03:19:57 +0200

    almost working ugly code

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ __version__ = 0.1
 
 from tabulate import tabulate
 from random import randrange
-from numpy import full, ndenumerate, count_nonzero, random
+from numpy import full, ndenumerate, count_nonzero, zeros
+
+DIRECTIONS = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
 
 
-def init_board(sizex: int, sizey: int) -> list:
+def init_board(sizex: int, sizey: int, zero=False) -> list:
     """
-    Take a sizex and sizey argument and create a nparray sizex x sizey
+    Take a sizex and sizey argument and create a nparray sizex x sizey with 0 if zero is True else -1
     """
     return full((sizey, sizex), -1)
 
@@ -35,20 +37,38 @@ def get_usrin() -> tuple:
     return coord
 
 
-def generate_bombs(board, nbr) -> list:
+def generate_bombs(board, nbr, usr_choice) -> list:
     """
     create nbr bombs in the board
     """
+    non = get_adjacent(board, usr_choice, index=True)
+    print(non)
     bombs_added = 0
     if count_nonzero(board == -1) < nbr:
         return board
     while nbr != bombs_added:
         rdm_y = randrange(0, len(board))
         rdm_x = randrange(0, len(board[0]))
-        if board[rdm_y, rdm_x] == -1:
+        print(rdm_y, rdm_x)
+        if board[rdm_y, rdm_x] == -1 and not (
+            non[0] <= rdm_y <= non[1] and non[2] <= rdm_x <= non[3]
+        ):
+            print("ok")
             board[rdm_y, rdm_x] = 9
             bombs_added += 1
     return board
+
+
+def get_adjacent(board, coord, index=False):
+    x_min = coord[1] - 1 if coord[1] - 1 > 0 else 0
+    x_max = (
+        coord[1] + 2 if coord[1] + 2 < len(board[coord[0]]) else len(board[coord[0]])
+    )
+    y_min = coord[0] - 1 if coord[0] - 1 > 0 else 0
+    y_max = coord[0] + 2 if coord[0] + 2 < len(board) + 1 else len(board) + 1
+    return (
+        board[y_min:y_max, x_min:x_max] if not index else [y_min, y_max, x_min, x_max]
+    )
 
 
 def create_numbers(board):
@@ -65,23 +85,55 @@ def create_numbers(board):
     return board
 
 
-
+# TODO : opti
 def make_group(board, coord):
-    """ """
-    oui=[]
-    for it, x in ndenumerate(board):
-        if x == 0:
-            
+    """
+    put all the indexs of adjacents 0 and first 0 on the coordinate selected
+    """
+    index_to_check = [coord]
+    reveal = [coord]
+    while len(index_to_check) > 0:
+        new_y, new_x = index_to_check.pop()
+        for y, x in DIRECTIONS:
+            neww_y, neww_x = new_y + y, new_x + x
+            if 0 <= neww_y < board.shape[0] and 0 <= neww_x < board.shape[1]:
+                if board[neww_y, neww_x] == 0 and (neww_y, neww_x) not in reveal:
+                    index_to_check.append((neww_y, neww_x))
+                if (neww_y, neww_x) not in reveal:
+                    reveal.append((neww_y, neww_x))
+    return reveal
 
-        
-        
+
+def reveal_board(board, index_to_reveal):
+    """ """
+    oui = init_board(board.shape[1], board.shape[0], True)
+    for i in index_to_reveal:
+        oui[i] = board[i]
+    return oui
+
 
 def main():
     """"""
+    blank_board = init_board(5, 5)
+    board = init_board(5, 5)
+    usr_choice = get_usrin()
+    board[usr_choice] = 15
+    create_numbers(generate_bombs(board, 5, usr_choice))
+    blank_board = reveal_board(board, make_group(board, usr_choice))
+    show_board(blank_board)
 
 
 if __name__ == "__main__":
-    oui = init_board(5, 5)
-    generate_bombs(oui, 3)
-    create_numbers(oui)
-    show_board(oui)
+    main()
+    # oui = init_board(5, 5)
+    # generate_bombs(oui, 3)
+    # create_numbers(oui)    oui = init_board(5, 5)
+    # generate_bombs(oui, 3)
+    # create_numbers(oui)
+    # show_board(oui)
+
+    # reveal_board(oui, make_group(oui, get_usrin()))
+
+    # show_board(oui)
+
+    # reveal_board(oui, make_group(oui, get_usrin()))
--- test_main.py
+++ test_main.py
@@ -5,7 +5,23 @@ __version__ = 0.1
 """
 
 import main
+import pytest
+import numpy as np
 
 
-def test_init_empty_board():
-    assert main.init_board(5, 5) == [[-1 for _ in range(5)] for _ in range(5)]
+@pytest.fixture
+def rand_width():
+    return np.random.randint(5, 20)
+
+
+@pytest.fixture
+def rand_height():
+    return np.random.randint(5, 20)
+
+
+@pytest.mark.loop(10)
+def test_init_empty_board(rand_width, rand_height):
+    board = main.init_board(rand_width, rand_height)
+    a, b = board.shape
+    assert b == rand_width
+    assert a == rand_height
