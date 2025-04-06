bcbdc85cc2516c59a96ca72292e40b8f5ab2a24a
gg <georgevanderveen@yahoo.fr>
2025-04-07 01:03:59 +0200

    presque

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ __version__ = 0.1
 
 from tabulate import tabulate
 from random import randrange
-from numpy import full, ndenumerate, count_nonzero, zeros
+from numpy import full, ndenumerate, count_nonzero, zeros, insert, arange
+from colorama import init, Fore, Back, Style
 
 DIRECTIONS = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
 
@@ -26,7 +27,17 @@ def show_board(board: list) -> None:
     """
     Print the board
     """
+    init()
+    headers_col = arange(0, board.shape[0])
+    headers_row = arange(-1, headers_col.shape[0] + 1)
+    board = insert(board, board.shape[1], headers_col, axis=0)
+    board = insert(board, 0, headers_col, axis=0)
+    board = insert(board, 0, headers_row, axis=1)
+    board = insert(board, board.shape[1], headers_row, axis=1)
+    print((f"{Fore.YELLOW}  {board[0]}  {Style.RESET_ALL}")
+
     print(tabulate(board, tablefmt="simple_grid"))
+    print(board)
 
 
 def get_usrin() -> tuple:
@@ -42,18 +53,15 @@ def generate_bombs(board, nbr, usr_choice) -> list:
     create nbr bombs in the board
     """
     non = get_adjacent(board, usr_choice, index=True)
-    print(non)
     bombs_added = 0
     if count_nonzero(board == -1) < nbr:
         return board
     while nbr != bombs_added:
         rdm_y = randrange(0, len(board))
         rdm_x = randrange(0, len(board[0]))
-        print(rdm_y, rdm_x)
         if board[rdm_y, rdm_x] == -1 and not (
             non[0] <= rdm_y <= non[1] and non[2] <= rdm_x <= non[3]
         ):
-            print("ok")
             board[rdm_y, rdm_x] = 9
             bombs_added += 1
     return board
@@ -99,28 +107,58 @@ def make_group(board, coord):
             if 0 <= neww_y < board.shape[0] and 0 <= neww_x < board.shape[1]:
                 if board[neww_y, neww_x] == 0 and (neww_y, neww_x) not in reveal:
                     index_to_check.append((neww_y, neww_x))
-                if (neww_y, neww_x) not in reveal:
+                if (neww_y, neww_x) not in reveal and board[(neww_y, neww_x)] != 9:
                     reveal.append((neww_y, neww_x))
     return reveal
 
 
-def reveal_board(board, index_to_reveal):
+def reveal_board(board, blank_board, index_to_reveal):
     """ """
-    oui = init_board(board.shape[1], board.shape[0], True)
     for i in index_to_reveal:
-        oui[i] = board[i]
-    return oui
+        blank_board[i] = board[i]
+    return blank_board
+
+
+def compare_boards(blank_board, board, coord) -> int:
+    """
+    return a int dor different case
+    -1 -> loose
+    0 -> already reveal
+    1 -> ok
+    """
+
+    if board[coord] == 9:
+        return -1
+    if board[coord] == blank_board[coord]:
+        return 0
+    else:
+        return 1
 
 
 def main():
     """"""
-    blank_board = init_board(5, 5)
-    board = init_board(5, 5)
-    usr_choice = get_usrin()
-    board[usr_choice] = 15
-    create_numbers(generate_bombs(board, 5, usr_choice))
-    blank_board = reveal_board(board, make_group(board, usr_choice))
+    blank_board = init_board(8, 8)
+    board = init_board(8, 8)
     show_board(blank_board)
+    usr_choice = get_usrin()
+    create_numbers(generate_bombs(board, 7, usr_choice))
+    blank_board = reveal_board(board, blank_board, make_group(board, usr_choice))
+    show_board(blank_board)
+    while True:
+        usr_choice = get_usrin()
+        check_cell = compare_boards(blank_board, board, usr_choice)
+        print(check_cell)
+        if check_cell == 1:
+            blank_board = reveal_board(
+                board, blank_board, make_group(board, usr_choice)
+            )
+            show_board(blank_board)
+        elif check_cell == 0:
+            print("case déjà révélé, réessayez ")
+        else:
+            show_board(board)
+            print("Perdu")
+            exit()
 
 
 if __name__ == "__main__":
